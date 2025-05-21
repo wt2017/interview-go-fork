618c920b64eda4817207aba947a45448b57fb3d5
wyou <wentao.you@nokia-sbell.com>
2025-05-21 08:30:57 +0300

    q007-wyou

--- src/q007-wyou.go
+++ src/q007-wyou.go
@@ -8,6 +8,19 @@ type Show struct {
 	Param
 }
 
+type student struct {
+	Name string
+}
+
+func zhoujielun(v interface{}) {
+	switch msg := v.(type) {
+	case student:
+		fmt.Println("struct type", msg.Name)
+	case *student:
+		fmt.Println("point type", msg.Name)
+	}
+}
+
 func main() {
 	s := Show{
 		Param: map[string]interface{}{
@@ -22,4 +35,7 @@ func main() {
 
 	fmt.Println(s.Param["key"])
 	fmt.Println(s2.Param["key2"])
+
+	zhoujielun(student{Name: "zhoujielun"})
+	zhoujielun(&student{Name: "zhoujielun"})
 }
